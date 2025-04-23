a42bf3dd0a189c5c298f9e1f01959275a6f4903d
Meltedvulture <0532946@schoolsnet.act.edu.au>
2025-04-23 13:46:50 +1000

    Buyable doors working
    Quick backup
    Migration of label and UI functions imminent

---
+++ Scripts/buyableDoor.gd
@@ -0,0 +1,22 @@
+extends StaticBody3D
+
+var objectType = "Door"
+@onready var animationPlayer = $AnimationPlayer
+@export var objectName : String = "Door"
+@export var cost = 1000
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func purchase():
+	if Global.points > cost:
+		Global.points -= cost
+		animationPlayer.play("open", -1, 1)
---
+++ controllers/scripts/interaction_cast.gd
@@ -0,0 +1,22 @@
+extends RayCast3D
+
+
+var collidedObject
+@onready var purchaseLabel = %InteractionLabel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	collidedObject = get_collider()
+	if collidedObject:
+		if collidedObject.is_in_group("BuyableObject"):
+			if collidedObject.objectType == "Door":
+				purchaseLabel.text = "Hold F to buy " + collidedObject.objectName + " Cost: " + str(collidedObject.cost)
+			if Input.is_action_just_pressed("interact"):
+				collidedObject.purchase()
+	else:
+		purchaseLabel.text = ""
